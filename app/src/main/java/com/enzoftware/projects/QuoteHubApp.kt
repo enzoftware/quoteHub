bf6d1ee3e143c18ccf37e37532eef67107c7d58d
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-15 00:19:06 -0500

    Add models

--- app/src/main/java/com/enzoftware/projects/QuoteHubApp.kt
+++ app/src/main/java/com/enzoftware/projects/QuoteHubApp.kt
@@ -4,4 +4,11 @@ import android.app.Application
 
 class QuoteHubApp : Application() {
 
+    companion object{
+        lateinit var instance : QuoteHubApp
+        private set
+
+    }
+
+
 }
---
+++ app/src/main/java/com/enzoftware/projects/model/RegisterModel.kt
@@ -0,0 +1,4 @@
+package com.enzoftware.projects.model
+
+class RegisterModel {
+}
