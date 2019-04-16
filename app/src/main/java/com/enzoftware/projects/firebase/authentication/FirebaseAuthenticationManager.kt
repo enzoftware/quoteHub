a7abce8a0b680375b39d941aea29102e5afc2f1b
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-15 19:31:17 -0500

    Add firebase implementation managers

---
+++ app/src/main/java/com/enzoftware/projects/firebase/authentication/FirebaseAuthenticationManager.kt
@@ -0,0 +1,25 @@
+package com.enzoftware.projects.firebase.authentication
+
+class FirebaseAuthenticationManager : FirebaseAuthentication {
+
+    override fun login(email: String, password: String, onResult: (Boolean) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getUserId(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getUserName(): String {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun logout(onResult: () -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun register(email: String, password: String, username: String, onResult: (Boolean) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
---
+++ app/src/main/java/com/enzoftware/projects/firebase/database/FirebaseDatabaseManager.kt
@@ -0,0 +1,32 @@
+package com.enzoftware.projects.firebase.database
+
+import com.enzoftware.projects.model.QuoteEntity
+import com.enzoftware.projects.model.UserEntity
+
+class FirebaseDatabaseManager : FirebaseDatabase {
+
+    override fun listenToQuotes(onResult: (QuoteEntity) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun addNewQuote(quoteEntity: QuoteEntity, onResult: (Boolean) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getFavoritesQuotes(userId: String, onResult: (List<QuoteEntity>) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun changeQuoteFavoriteStatus(userId: String, quoteEntity: QuoteEntity) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createUser(id: String, email: String, username: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getProfile(id: String, onResult: (UserEntity) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
