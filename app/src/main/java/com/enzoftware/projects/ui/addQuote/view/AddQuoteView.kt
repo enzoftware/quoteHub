06537e17db6270e24a8d25e61da9031422de323b
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 16:18:38 -0500

    Add welcome implements mvp

---
+++ app/src/main/java/com/enzoftware/projects/ui/addQuote/view/AddQuoteView.kt
@@ -0,0 +1,13 @@
+package com.enzoftware.projects.ui.addQuote.view
+
+interface AddQuoteView {
+
+    fun onQuoteAdd()
+
+    fun showAddQuoteError()
+
+    fun showQouteError()
+
+    fun removeQuoteError()
+
+}
---
+++ app/src/main/java/com/enzoftware/projects/ui/welcome/view/WelcomeView.kt
@@ -0,0 +1,7 @@
+package com.enzoftware.projects.ui.welcome.view
+
+interface WelcomeView {
+
+    fun startMainScreen()
+
+}
