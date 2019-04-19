c836cfaabc6d14620302625f8e4167b87616397c
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 18:15:22 -0500

    home page adapter

--- app/src/main/java/com/enzoftware/projects/ui/home/HomePagerAdapter.kt
+++ app/src/main/java/com/enzoftware/projects/ui/home/HomePagerAdapter.kt
@@ -1,2 +1,25 @@
 package com.enzoftware.projects.ui.home
 
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class HomePagerAdapter(manager: FragmentManager) : FragmentPagerAdapter(manager) {
+
+    private val pages = mutableListOf<Fragment>()
+
+    fun setPages(data: List<Fragment>) {
+        pages.clear()
+        pages.addAll(data)
+        notifyDataSetChanged()
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+}
