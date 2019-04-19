9cf35470fd8bb9970d1603ee58bf4af5bd2915bb
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 18:10:38 -0500

    add application providers

---
+++ app/src/main/java/com/enzoftware/projects/di/ApplicationProviders.kt
@@ -0,0 +1,19 @@
+package com.enzoftware.projects.di
+
+import com.enzoftware.projects.QuoteHubApp
+
+private fun component() = QuoteHubApp.component
+
+fun loginPresenter() = component().loginPresenter()
+
+fun registerPresenter() = component().registerPresenter()
+
+fun allQuotesPresenter() = component().allQuotesPresenter()
+
+fun favoriteQuotesPresenter() = component().favoriteQuotesPresenter()
+
+fun profilePresenter() = component().profilePresenter()
+
+fun addJokePresenter() = component().addQuotePresenter()
+
+fun welcomePresenter() = component().welcomePresenter()
