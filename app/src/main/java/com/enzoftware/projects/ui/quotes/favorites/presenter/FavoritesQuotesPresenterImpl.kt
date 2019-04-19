8528f9a40d5c89870bd70b2ee2a375a0035019a1
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 18:15:01 -0500

    quotes fav presenter impl

--- app/src/main/java/com/enzoftware/projects/ui/quotes/favorites/presenter/FavoritesQuotesPresenterImpl.kt
+++ app/src/main/java/com/enzoftware/projects/ui/quotes/favorites/presenter/FavoritesQuotesPresenterImpl.kt
@@ -1,2 +1,20 @@
 package com.enzoftware.projects.ui.quotes.favorites.presenter
 
+import com.enzoftware.projects.model.QuoteEntity
+import com.enzoftware.projects.ui.quotes.favorites.view.FavoriteQuoteView
+
+class FavoritesQuotesPresenterImpl : FavoritesQuotesPresenter {
+
+    override fun getFavoritesQuotes() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onFavoriteButtonTapped(quote: QuoteEntity) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun setView(view: FavoriteQuoteView) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
