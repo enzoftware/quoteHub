cd9d173fccea632de8f0bb72ed9bf30a560d027a
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 22:31:15 -0500

    fix home activity

--- app/src/main/java/com/enzoftware/projects/ui/home/HomeActivity.kt
+++ app/src/main/java/com/enzoftware/projects/ui/home/HomeActivity.kt
@@ -5,20 +5,12 @@ import android.view.Menu
 import android.view.MenuItem
 import com.enzoftware.projects.R
 import com.enzoftware.projects.ui.base.BaseActivity
-import com.google.android.material.snackbar.Snackbar
-import kotlinx.android.synthetic.main.activity_main.*
 
 class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
