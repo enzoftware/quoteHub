091f16a53999b03b7e0020ccb1e85ca0b520ec03
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 22:29:38 -0500

    add quote present impl

--- app/src/main/java/com/enzoftware/projects/ui/addQuote/presenter/AddQuotePresenterImpl.kt
+++ app/src/main/java/com/enzoftware/projects/ui/addQuote/presenter/AddQuotePresenterImpl.kt
@@ -1,19 +1,51 @@
 package com.enzoftware.projects.ui.addQuote.presenter
 
+import com.enzoftware.projects.common.isValidQuote
+import com.enzoftware.projects.firebase.authentication.FirebaseAuthenticationInterface
+import com.enzoftware.projects.firebase.database.FirebaseDatabaseInterface
+import com.enzoftware.projects.model.QuoteEntity
 import com.enzoftware.projects.ui.addQuote.view.AddQuoteView
+import javax.inject.Inject
 
-class AddQuotePresenterImpl : AddQuotePresenter {
+class AddQuotePresenterImpl @Inject constructor(
+    private val authenticationInterface: FirebaseAuthenticationInterface,
+    private val databaseInterface: FirebaseDatabaseInterface
+) : AddQuotePresenter {
+
+    private lateinit var view: AddQuoteView
+
+    private var quoteText: String = ""
 
     override fun addQuote() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (isValidQuote(quoteText)) {
+            val quote =
+                QuoteEntity("", authenticationInterface.getUserName(), authenticationInterface.getUserId(), quoteText)
+            databaseInterface.addNewQuote(quote) {
+                onAddQuoteResult(it)
+            }
+        }
     }
 
     override fun onQuoteTextChanged(quoteText: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.quoteText = quoteText
+
+        if (!isValidQuote(quoteText)) {
+            view.showQouteError()
+        } else {
+            view.removeQuoteError()
+        }
     }
 
     override fun setView(view: AddQuoteView) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.view = view
+    }
+
+    private fun onAddQuoteResult(isSuccess: Boolean) {
+        if (isSuccess) {
+            view.onQuoteAdd()
+        } else {
+            view.showAddQuoteError()
+        }
     }
 
 }
