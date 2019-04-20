d9a929dffcdc981889a37754d82f47b812f2960f
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 22:30:30 -0500

    register implementation

--- app/src/main/java/com/enzoftware/projects/ui/register/presenter/RegisterPresenterImpl.kt
+++ app/src/main/java/com/enzoftware/projects/ui/register/presenter/RegisterPresenterImpl.kt
@@ -1,31 +1,83 @@
 package com.enzoftware.projects.ui.register.presenter
 
+import com.enzoftware.projects.common.arePasswordsSame
+import com.enzoftware.projects.common.isEmailValid
+import com.enzoftware.projects.common.isPasswordValid
+import com.enzoftware.projects.common.isUsernameValid
+import com.enzoftware.projects.firebase.authentication.FirebaseAuthenticationInterface
+import com.enzoftware.projects.firebase.database.FirebaseDatabaseInterface
+import com.enzoftware.projects.model.RegisterModel
 import com.enzoftware.projects.ui.register.view.RegisterView
+import javax.inject.Inject
 
-class RegisterPresenterImpl : RegisterPresenter {
+class RegisterPresenterImpl @Inject constructor(
+    private val databaseInterface: FirebaseDatabaseInterface,
+    private val authenticationInterface: FirebaseAuthenticationInterface
+) : RegisterPresenter {
+
+    private lateinit var view: RegisterView
+
+    private val userData = RegisterModel()
 
     override fun onUsernameChanged(username: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        userData.username = username
+
+        if (!isUsernameValid(username)) {
+            view.showUsernameError()
+        }
     }
 
     override fun onEmailChanged(email: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        userData.email = email
+
+        if (!isEmailValid(email)) {
+            view.showEmailError()
+        }
     }
 
     override fun onPasswordChanged(password: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        userData.password = password
+
+        if (!isPasswordValid(password)) {
+            view.showPasswordError()
+        }
     }
 
     override fun onRepeatPasswordChanged(repeatPassword: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        userData.repeatPassword = repeatPassword
+
+        if (!arePasswordsSame(userData.password, repeatPassword)) {
+            view.showPasswordMatchingError()
+        }
     }
 
     override fun onRegisterClicked() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (userData.isValid()) {
+            val (name, email, password) = userData
+            authenticationInterface.register(email, password, name) { isSuccessful ->
+                onRegisterResult(isSuccessful, name, email)
+            }
+        }
     }
 
     override fun setView(view: RegisterView) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.view = view
+    }
+
+    private fun onRegisterResult(isSuccessful: Boolean, username: String, email: String) {
+        if (!isSuccessful) {
+            createUser(username, email)
+            view.onRegisterSuccess()
+        } else {
+            view.showSignUpError()
+        }
+    }
+
+    private fun createUser(username: String, email: String) {
+        val id = authenticationInterface.getUserId()
+
+        databaseInterface.createUser(id, username, email)
+
     }
 
 }
