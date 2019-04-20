5ad42cc29d332839714fe6af211c5ef60aac19ab
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 22:31:01 -0500

    login implementation

--- app/src/main/java/com/enzoftware/projects/ui/login/presenter/LoginPresenterImpl.kt
+++ app/src/main/java/com/enzoftware/projects/ui/login/presenter/LoginPresenterImpl.kt
@@ -1,23 +1,50 @@
 package com.enzoftware.projects.ui.login.presenter
 
+import com.enzoftware.projects.common.isEmailValid
+import com.enzoftware.projects.common.isPasswordValid
+import com.enzoftware.projects.firebase.authentication.FirebaseAuthenticationInterface
+import com.enzoftware.projects.model.LoginModel
 import com.enzoftware.projects.ui.login.view.LoginView
+import javax.inject.Inject
 
-class LoginPresenterImpl : LoginPresenter {
+class LoginPresenterImpl @Inject constructor(
+    private val authentication: FirebaseAuthenticationInterface
+) : LoginPresenter {
+
+    private lateinit var view: LoginView
+
+    private val loginRequest = LoginModel()
 
     override fun onLoginClicked() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        if (loginRequest.isValid()) {
+            authentication.login(loginRequest.email, loginRequest.password) { isSuccess ->
+                if (isSuccess) {
+                    view.onLoginSuccess()
+                } else {
+                    view.onLoginError()
+                }
+            }
+        }
     }
 
     override fun onEmailChanged(email: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        loginRequest.email = email
+
+        if (!isEmailValid(email)) {
+            view.showEmailError()
+        }
     }
 
     override fun onPasswordChanged(password: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        loginRequest.password = password
+
+        if (!isPasswordValid(password)) {
+            view.showPasswordError()
+        }
     }
 
     override fun setView(view: LoginView) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        this.view = view
     }
 
 }
