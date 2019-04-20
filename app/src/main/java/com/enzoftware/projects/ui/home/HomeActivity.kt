d9e981715bf1bd9123974100f3409ed40e839a35
Enzo Lizama <lizama.enzo@gmail.com>
2019-04-19 22:32:01 -0500

    fix home activity

--- app/src/main/java/com/enzoftware/projects/ui/home/HomeActivity.kt
+++ app/src/main/java/com/enzoftware/projects/ui/home/HomeActivity.kt
@@ -10,7 +10,7 @@ class HomeActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_home)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
