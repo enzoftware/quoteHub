7f737417cff8e883a562950168089671b26a52d2
Enzo Lizama <lizama.enzo@gmail.com>
2019-07-24 01:30:44 -0500

    databse firebase impl

--- app/src/main/java/com/enzoftware/projects/firebase/database/FirebaseDatabaseManager.kt
+++ app/src/main/java/com/enzoftware/projects/firebase/database/FirebaseDatabaseManager.kt
@@ -1,13 +1,32 @@
 package com.enzoftware.projects.firebase.database
 
-import com.enzoftware.projects.model.QuoteEntity
-import com.enzoftware.projects.model.UserEntity
+import com.enzoftware.projects.model.*
+import com.google.firebase.database.*
 import javax.inject.Inject
 
-class FirebaseDatabaseManager @Inject constructor() : FirebaseDatabaseInterface {
+private const val KEY_USER = "user"
+private const val KEY_QUOTE = "quote"
+private const val KEY_FAV = "favorite"
+
+
+class FirebaseDatabaseManager @Inject constructor(private val database: FirebaseDatabase) : FirebaseDatabaseInterface {
 
     override fun listenToQuotes(onResult: (QuoteEntity) -> Unit) {
-
+        database.reference.child(KEY_QUOTE)
+            .orderByKey()
+            .addChildEventListener(object : ChildEventListener{
+                override fun onCancelled(p0: DatabaseError) = Unit
+                override fun onChildMoved(p0: DataSnapshot, p1: String?) = Unit
+                override fun onChildChanged(p0: DataSnapshot, p1: String?) = Unit
+                override fun onChildRemoved(p0: DataSnapshot) = Unit
+                override fun onChildAdded(snapshot: DataSnapshot, p1: String?) {
+                    snapshot.getValue(QuoteResponse::class.java)?.run {
+                        if(isValid()){
+                            onResult(mapToQuote())
+                        }
+                    }
+                }
+            })
     }
 
     override fun addNewQuote(quoteEntity: QuoteEntity, onResult: (Boolean) -> Unit) {
@@ -23,11 +42,26 @@ class FirebaseDatabaseManager @Inject constructor() : FirebaseDatabaseInterface 
     }
 
     override fun createUser(id: String, email: String, username: String) {
-
+        val user = UserEntity(id, email, username)
+        database.reference.child(KEY_USER).child(id).setValue(user)
     }
 
     override fun getProfile(id: String, onResult: (UserEntity) -> Unit) {
-
+        database.reference
+            .child(KEY_USER)
+            .child(id)
+            .addValueEventListener(object : ValueEventListener{
+                override fun onCancelled(p0: DatabaseError) = Unit
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val user = snapshot.getValue(UserEntityResponse::class.java)
+                    val favoritesJokes = snapshot.child(KEY_FAV).children
+                        .map { it.getValue(QuoteResponse::class.java) }
+                        .mapNotNull { it?.mapToQuote() }
+                    user?.run {
+                        onResult(UserEntity(id, username, email, favoritesJokes))
+                    }
+                }
+            })
     }
 
 }
--- app/src/main/java/com/enzoftware/projects/model/QuoteEntity.kt
+++ app/src/main/java/com/enzoftware/projects/model/QuoteEntity.kt
@@ -7,3 +7,16 @@ data class QuoteEntity(
     val text: String,
     var isFavorite: Boolean = false
 )
+
+
+data class QuoteResponse(val id: String = "",
+                        val authorName: String = "",
+                        val authorId: String = "",
+                        val text: String = "")
+
+fun QuoteResponse.isValid() = id.isNotBlank()
+        && authorName.isNotBlank()
+        && authorId.isNotBlank()
+        && text.isNotBlank()
+
+fun QuoteResponse.mapToQuote() = QuoteEntity(id, authorName, authorId, text)
